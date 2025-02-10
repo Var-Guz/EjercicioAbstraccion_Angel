7eec3bb2d352e72e5b4d66eb9564d0f68730bb4b
Var-Guz <davidvarguz.7@unitec.edu>
2025-02-09 23:42:17 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioAbstraccion_Angel/AreaTriangulo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace EjercicioAbstraccion_Angel
+{
+    public class AreaTriangulo : Formulas
+    {
+        private double baseTriangulo;
+        private double alturaTriangulo;
+
+    }
+
+     public AreaTriangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.alturaTriangulo = alturaTriangulo;
+        }
+
+
+
+    }
---
+++ EjercicioAbstraccion_Angel/Formulas.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EjercicioAbstraccion_Angel
+{
+    public abstract class Formulas
+    {
+        public abstract double Calcular();
+        public abstract void Imprimir();
+
+
+    }
+}
